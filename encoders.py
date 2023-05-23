45fc5463ca04698d8cd59c78e43f88dc0aeee99c
Abdullah Abdulrahman <zerkclown@gmail.com>
2023-05-23 16:27:41 -0400

    small syntactic fixes

--- encoders.py
+++ encoders.py
@@ -1,5 +1,4 @@
 import torch.nn as nn
-import torch.cat as cat
 import torch
 import torch.nn.functional as F
 import torchaudio as ta
@@ -85,8 +84,8 @@ class ExpressionEncoder(nn.Module):
     """
     ExpressionEncoder to extract a latent space from 3D mesh data
     """
-    def __init__(self):
-        super(ExpressionEncoder, self, latent_dim = 128, n_vertices = 6172).__init__()
+    def __init__(self, latent_dim = 128, n_vertices = 6172):
+        super(ExpressionEncoder, self).__init__()
 
         self.fc1 = nn.Linear(n_vertices * 3, 256)
         self.fc2 = nn.Linear(256, 128)
@@ -110,14 +109,14 @@ class FusionEncoder():
     """
     FusionEncoder to extract a combined latent space from both of audio and expression latent spaces
     """
-    def __init__(self):
-        super(ExpressionEncoder, self, latent_dim = 2*128, heads = 64, categories = 128).__init__()
+    def __init__(self, latent_dim = 2*128, heads = 64, categories = 128):
+        super(ExpressionEncoder, self).__init__()
         
         # using a simple 3 layers MLP block to combine latent spaces
         self.mlp = nn.Sequential(
                 nn.Linear(latent_dim, latent_dim),
                 nn.LeakyReLU(0.2),
-                nn.Linear(latent_dim, latent_dim)
+                nn.Linear(latent_dim, latent_dim),
                 nn.LeakyReLU(0.2),
                 nn.Linear(latent_dim, output_size),
             )
@@ -125,7 +124,7 @@ class FusionEncoder():
     def forward(self, audioSpace, expressionSpace):
         
         # concat latent spaces
-        x = cat((expressionSpace, audioSpace), -1)
+        x = torch.cat((expressionSpace, audioSpace), -1)
         x = self.mlp(x).reshape(x.shape[0], x.shape[1], self.head, self.categories)
         x = F.gumbel_softmax(x, tau=2, hard=False, dim=-1) #TODO: test tau=1?
 
