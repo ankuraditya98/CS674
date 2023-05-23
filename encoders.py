c3c90fe8322c132574b4bb1ced4d28549d9a6d5e
Abdullah Abdulrahman <zerkclown@gmail.com>
2023-05-23 00:34:27 -0400

    added code that implements the fusion encoder

--- encoders.py
+++ encoders.py
@@ -1,12 +1,13 @@
 import torch.nn as nn
+import torch.cat as cat
 
 
 class ExpressionEncoder(nn.Module):
     """
-    ExpressionEncoder to extract latent spaced from 3D mesh data
+    ExpressionEncoder to extract a latent space from 3D mesh data
     """
     def __init__(self):
-        super(ExpressionEncoder, self, latent_dim = 128, n_vertices = 6172, model_name = 'expression_encoder').__init__()
+        super(ExpressionEncoder, self, latent_dim = 128, n_vertices = 6172).__init__()
 
         self.fc1 = nn.Linear(n_vertices * 3, 256)
         self.fc2 = nn.Linear(256, 128)
@@ -26,3 +27,27 @@ class ExpressionEncoder(nn.Module):
 
         return x
         
+class FusionEncoder():
+    """
+    FusionEncoder to extract a combined latent space from both of audio and expression latent spaces
+    """
+    def __init__(self):
+        super(ExpressionEncoder, self, latent_dim = 2*128, heads = 64, categories = 128).__init__()
+        
+        # using a simple 3 layers MLP block to combine latent spaces
+        self.mlp = nn.Sequential(
+                nn.Linear(latent_dim, latent_dim),
+                nn.LeakyReLU(0.2),
+                nn.Linear(latent_dim, latent_dim)
+                nn.LeakyReLU(0.2),
+                nn.Linear(latent_dim, output_size),
+            )
+
+    def forward(self, audioSpace, expressionSpace):
+        
+        # concat latent spaces
+        x = cat((expressionSpace, audioSpace), -1)
+        x = self.mlp(x).reshape(x.shape[0], x.shape[1], self.head, self.categories)
+        x = nn.functional.gumbel_softmax(x, tau=2, hard=False, dim=-1) #TODO: test tau=1?
+
+        return x
